122c25ea66feb6b612379aa64e2bff2a92a7f238
Thomas Taylor <dev@taylortom.co.uk>
2014-09-29 18:11:35 +0100

    Code refactor mkI
    Too many changes to list separately, no difference in functionality, just moving stuff around,
    hopefully making code an easier read.

--- js/adapt-timed-sequence.js
+++ js/adapt-timed-sequence.js
@@ -1,8 +1,8 @@
 
 define(function(require) {
 
-	var QuestionView = require('coreViews/questionView');
-	var Adapt = require('coreJS/adapt');
+	var QuestionView = require("coreViews/questionView");
+	var Adapt = require("coreJS/adapt");
 
 	var TimedSequence = QuestionView.extend({
 
@@ -13,96 +13,40 @@ define(function(require) {
 
 		preRender:function(){
 			QuestionView.prototype.preRender.apply(this);
-			this.listenTo(Adapt, 'device:changed', this.handleDeviceChanged);
-			this.listenTo(Adapt, 'device:resize', this.handleDeviceResize);
+			this.listenTo(Adapt, "device:changed", this.onDeviceChanged);
+			this.listenTo(Adapt, "device:resize", this.onDeviceResized);
 		},
 
 		postRender: function() {
 			QuestionView.prototype.postRender.apply(this);
-			this.$('.timed-sequence-widget').imageready(_.bind(function() {
-				this.setupLayout();
-				this.setupSequence();
-				this.setReadyStatus();
-			}, this));
-
-			this.userAnswersArray = [];
-			this.model.set({
-				_userAnswers:this.userAnswersArray
-			});
-
-			// removed so timed sequence resets left incase we need to go back
-			//if(!this.model.get("_isComplete")) {
-			//    this.userAnswersArray = [];
-			//    this.model.set({
-			//        _userAnswers:this.userAnswersArray
-			//    });
-			//} else {
-			//    this.showCompletedState();
-			//}
-		},
-
-		handleDeviceChanged: function() {
-			this.setupLayout();
-		},
-
-		handleDeviceResize: function() {
-			this.setupLayout();
+			this.$(".timed-sequence-widget").imageready(_.bind(this.onWidgetImageReady, this));
 		},
 
 		setupLayout: function() {
 			this.width = this.$(".sequence-container").width();
-			this.$(".sequence-container-inner").css({
-				width:this.width * this.model.get("_items").length
-			});
-			this.$(".sequence-image").css({
-				width:this.width
-			});
+			this.$(".sequence-container-inner").css("width", this.width * this.model.get("_items").length);
+			this.$(".sequence-image").css("width", this.width);
 		},
 
 		setupSequence: function() {
+			this.model.set({
+				_currentStageIndex: 0,
+				_correctAnswers: 0,
+				_incorrectAnswers: 0,
+				_currentAnswer: undefined,
+			});
+
 			this.setupSequenceIndicators();
-			this.correctAnswers = 0;
-			this.incorrectAnswers = 0;
-			this.currentAnswer;
 		},
 
 		setupSequenceIndicators: function() {
 			var itemsLength = this.model.get("_items").length
-			this.$(".sequence-indicator").css({
-				width:(100 / itemsLength) + "%"
-			});
+			this.$(".sequence-indicator").css("width", (100/itemsLength) + "%");
 		},
 
-		onStartClicked: function(event) {
-			if (event) event.preventDefault();
-			this.$(".sequence-state-container").velocity({
-				top:"-100%"
-			},{
-				duration:800,
-				easing:"swing"
-			});
-			this.$(".sequence-start-button").removeClass("show");
-			this.$(".sequence-answer-button").addClass("show");
-			this.stage = 0;
-			this.startTimer();
-			this.updateIndicator();
-		},
-
-		onAnswerClicked: function(event) {
-			if (event) event.preventDefault();
-			if (this.currentAnswer == this.stage) {
-				return;
-			}
-			this.currentAnswer = this.stage;
-			this.stopTimer();
-			this.endCurrentStage();
-		},
-
-		startTimer:function() {
-			var timerInterval = this.model.get("_timerInterval") * 1000;
-			this.timer = setInterval(_.bind(function() {
-				this.updateSequence();
-			},this), timerInterval);
+		startTimer: function() {
+			var timerInterval = this.model.get("_timerInterval")*1000;
+			this.timer = setInterval(_.bind(this.onTimerInterval,this), timerInterval);
 		},
 
 		stopTimer: function() {
@@ -111,157 +55,101 @@ define(function(require) {
 
 		updateSequence: function() {
 			this.checkUserAnswer();
-			if (this.stage == this.model.get("_items").length - 1) {
-				this.endSequence();
-			} else {
-				this.showNextImage();
-			}
+
+			var atLastStage = this.model.get("_currentStageIndex") == this.model.get("_items").length-1;
+			if (atLastStage) this.endSequence();
+			else this.showNextImage();
 		},
 
 		showNextImage: function() {
-			this.stage++;
-			this.$('.sequence-container-inner').velocity({
-				marginLeft:-(this.stage * this.width) + "px"
-			});
+			this.model.set("_currentStageIndex", this.model.get("_currentStageIndex")+1);
+			var leftMarg = -(this.model.get("_currentStageIndex") * this.width);
+			this.$(".sequence-container-inner").velocity({ marginLeft: leftMarg + "px" });
 			this.updateIndicator();
 		},
 
 		updateIndicator: function() {
-			var timerInterval = this.model.get("_timerInterval") * 1000;
-			var $indicator = this.$(".sequence-indicator").eq(this.stage);
+			var timerInterval = this.model.get("_timerInterval")*1000;
+			var $indicator = this.$(".sequence-indicator").eq(this.model.get("_currentStageIndex"));
 			var $indicatorInner = $indicator.children(".sequence-indicator-inner");
-			$indicatorInner.animate({
-				width:"100%"
-			}, timerInterval);
+			$indicatorInner.animate({ width:"100%" }, timerInterval);
 		},
 
 		endSequence: function() {
-			var that = this;
 			this.stopTimer();
 			this.$(".sequence-state-container").addClass("complete");
 			this.$(".sequence-answer-button").removeClass("show");
 			this.$(".sequence-complete-button").addClass("show");
-			this.$(".sequence-state-container").velocity("reverse", function() {
-				that.readyForMarking();
-			});
-		},
-
-		readyForMarking: function() {
-			if (this.isCorrect()) {
-				this.onQuestionCorrect();
-			} else {
-				this.onQuestionIncorrect();
-			}
-
-			this.setCompletionStatus();
-			this.showFeedback();
+			this.$(".sequence-state-container").velocity("reverse", _.bind(this.onQuestionComplete, this));
 		},
 
 		isCorrect: function() {
-			return this.correctAnswers == this.model.get("_items").length;
+			return this.model.get("_correctAnswers") == this.model.get("_items").length;
 		},
 
 		isPartlyCorrect: function() {
-			return this.incorrectAnswers <= this.model.get("_answerLeniency");
+			return this.model.get("_incorrectAnswers") <= this.model.get("_answerLeniency");
 		},
 
 		checkUserAnswer: function() {
-			var userDidInteract = this.checkUserInteraction();
-			var answer = this.markAnswer(userDidInteract);
+			var isCorrect = this.markAnswer(this.userDidInteract());
+			var userAnswer = { _isCorrect:isCorrect };
 
-			var userAnswer = {
-				_isCorrect:answer
-			};
+			this.model.get("_userAnswers").push(userAnswer);
 
-			this.userAnswersArray.push(userAnswer);
-			this.model.set({
-				_userAnswers:this.userAnswersArray
-			});
-
-			this.updateAnswerCounters(answer);
+			this.updateAnswerCounters(isCorrect);
 			this.showIndicatorMarking();
 			this.showSequenceFeedback(userAnswer);
 		},
 
-		checkUserInteraction: function() {
-			if (this.currentAnswer == this.stage) {
-				return true;
-			} else {
-				return false;
-			}
+		userDidInteract: function() {
+			return this.model.get("_currentAnswer") === this.model.get("_currentStageIndex");
 		},
 
 		markAnswer: function(userDidInteract) {
-			var shouldBeSelected = this.model.get("_items")[this.stage]._shouldBeSelected;
-			if (userDidInteract) {
-				if (shouldBeSelected) {
-					return true;
-				} else {
-					return false;
-			}
-			} else if (!userDidInteract) {
-				if (shouldBeSelected) {
-					return false;
-				} else {
-					return true;
-				}
-			}
+			var shouldBeSelected = this.model.get("_items")[this.model.get("_currentStageIndex")]._shouldBeSelected;
+			var correctInteraction = (userDidInteract && shouldBeSelected) || (!userDidInteract && !shouldBeSelected);
+			return correctInteraction;
 		},
 
-		updateAnswerCounters: function(answer) {
-			if(answer) {
-				this.correctAnswers++;
-			} else {
-				this.incorrectAnswers++;
-			}
+		updateAnswerCounters: function(isCorrect) {
+			if(isCorrect) this.model.set("_correctAnswers", this.model.get("_correctAnswers")+1);
+			else this.model.set("_incorrectAnswers", this.model.get("_incorrectAnswers"));
 		},
 
 		showIndicatorMarking: function() {
 			_.each(this.model.get("_userAnswers"), _.bind(function(item, index) {
 				var $indicator = this.$(".sequence-indicator").eq(index);
-				if (item._isCorrect) {
-					$indicator.children(".icon-tick").addClass("show");
-				} else {
-					$indicator.children(".icon-cross").addClass("show");
-				}
+				var iconClass = (item._isCorrect) ? ".icon-tick" : ".icon-cross";
+				$indicator.children(iconClass).addClass("show");
 			}, this));
 		},
 
 		showSequenceFeedback: function(userAnswer) {
 			var $feedbackContainer = this.$(".sequence-feedback-container");
-			var $feedbackIcon;
-			if (userAnswer._isCorrect) {
-				$feedbackIcon = $feedbackContainer.children(".icon-tick");
-			} else {
-				$feedbackIcon = $feedbackContainer.children(".icon-cross");
-			}
-			this.animateFeedbackIcon($feedbackIcon);
+			var iconClass = (userAnswer._isCorrect) ? ".icon-tick" : ".icon-cross";
+			this.animateFeedbackIcon($feedbackContainer.children(iconClass));
 		},
 
 		animateFeedbackIcon: function($element) {
-			$element.velocity({
-					opacity: 1
-			}, 50, function() {
-				$element.velocity({
-					opacity: 0
-				}, 500);
+			// quickly fade in, then fade out immediately
+			$element.velocity({ opacity: 1 }, 50, function() {
+				$element.velocity({ opacity: 0 }, 500);
 			});
         },
 
 		endCurrentStage: function() {
 			this.checkUserAnswer();
-			var $indicator = this.$(".sequence-indicator").eq(this.stage);
-			$indicator.children(".sequence-indicator-inner").stop().animate({
-				width:100 + "%"
-			}, 500, _.bind(function() {
-				this.startFromNextStage();
-			}, this));
+			var $indicator = this.$(".sequence-indicator").eq(this.model.get("_currentStageIndex"));
+			$indicator.children(".sequence-indicator-inner").stop().animate({ width:"100%" }, 500, _.bind(this.startFromNextStage, this));
 		},
 
 		startFromNextStage: function() {
-			if (this.stage == this.model.get("_items").length - 1) {
+			var atLastStage = this.model.get("_currentStageIndex") == this.model.get("_items").length-1;
+			if (atLastStage) {
 				this.endSequence();
-			} else {
+			}
+			else {
 				this.showNextImage();
 				this.startTimer();
 			}
@@ -271,10 +159,58 @@ define(function(require) {
 			this.$(".sequence-state-container").addClass("complete");
 			this.$(".sequence-answer-button, .sequence-start-button").removeClass("show");
 			this.$(".sequence-complete-button").addClass("show");
-			this.$(".sequence-indicator-inner").css({
-				width:100 + "%"
-			});
+			this.$(".sequence-indicator-inner").css("width", "100%");
 			this.showIndicatorMarking();
+		},
+
+		/**
+		* Event handling
+		*/
+
+		onDeviceChanged: function() { this.setupLayout(); },
+		onDeviceResized: function() { this.setupLayout(); },
+
+		onWidgetImageReady: function() {
+			this.model.set("_userAnswers", []);
+			this.setupLayout();
+			this.setupSequence();
+			this.setReadyStatus();
+		},
+
+		onStartClicked: function(event) {
+			if (event) event.preventDefault();
+
+			this.$(".sequence-state-container").velocity({ top:"-100%" },{ duration:800, easing:"swing" });
+			this.$(".sequence-start-button").removeClass("show");
+			this.$(".sequence-answer-button").addClass("show");
+
+			this.startTimer();
+			this.updateIndicator();
+		},
+
+		onAnswerClicked: function(event) {
+			if (event) event.preventDefault();
+
+			if (this.model.get("_currentAnswer") == this.model.get("_currentStageIndex")) return;
+			this.model.set("_currentAnswer", this.model.get("_currentStageIndex"));
+			this.stopTimer();
+			this.endCurrentStage();
+		},
+
+		onQuestionComplete: function() {
+			this.setCompletionStatus();
+
+			this.updateAttempts();
+			this.setQuestionAsSubmitted();
+			this.storeUserAnswer();
+			this.markQuestion();
+			this.setScore();
+			this.setupFeedback();
+			this.showFeedback();
+		},
+
+		onTimerInterval: function() {
+			this.updateSequence();
 		}
 	});
 
